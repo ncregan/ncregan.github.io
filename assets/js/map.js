4a27ed030949dd73147cbae3615aebcf7bbda00c
Nick Cregan <ncregan@mines.edu>
2023-09-27 12:34:07 -0600

    added map

---
+++ assets/js/map.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 6,
+      center: {lat: 39.1131, lng: -105.3588}
+    });
+});
